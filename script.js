2b7f6a92af2f2a6fb417ec606b96f486bb477ece
Svetlana Kuznetsova <KuznetsovaSveta0408@gmail.com>
2023-10-24 14:18:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', () => {
+    function calculateTime() {
+        let currentDate = new Date();
+        let year = currentDate.getFullYear();
+        let month = currentDate.getMonth() + 1;
+        let day = currentDate.getDay();
+        let num = currentDate.getDate();
+        let hours = currentDate.getHours();
+        let minutes = currentDate.getMinutes();
+        let seconds = currentDate.getSeconds();
+
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        document.querySelector('.clock__date-num').innerHTML = num + '.';
+        document.querySelector('.clock__date-month').innerHTML = month + '.';
+        document.querySelector('.clock__date-year').innerHTML = year;
+        document.querySelector('.clock__date-day').innerHTML = days[day];
+
+        document.querySelector('.clock__item-hours').innerHTML = hours;
+        document.querySelector('.clock__item-minutes').innerHTML = minutes;
+        document.querySelector('.clock__item-seconds').innerHTML = seconds;
+
+        setTimeout(calculateTime, 100);
+    }
+    calculateTime()
+
+    let menuBtn = document.querySelector('.menu__btn');
+    let menuWrapper = document.querySelector('.menu__wrapper');
+
+    menuBtn.addEventListener('click', () => {
+        menuWrapper.classList.toggle('active')
+    })
+
+    let clock = document.querySelector('.clock');
+    let inputTextColor = document.querySelector('#textColor');
+    let inputBackgroundColor = document.querySelector('#backgroundColor');
+
+    inputTextColor.addEventListener('input', () => {
+        clock.style.color = inputTextColor.value;
+    });
+    inputBackgroundColor.addEventListener('input', () => {
+        clock.style.background = inputBackgroundColor.value;
+    });
+});
+
